a87b8e136613006823febb2fc0b63ccd869857d1
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-01-10 18:14:18 +0900

    Web Crawling finished

---
+++ 2022 LL/Python/inten/crawling.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+url = "https://www.naver.com/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+menus = soup.findAll("a","nav")
+file = open("NaverMenu.txt","a",encoding="UTF-8")
+
+dateInfo = datetime.today().strftime("%Y년 %m월 %d일의 네이버 메뉴입니다. \n")
+file.write(dateInfo)
+print(dateInfo)
+for index, menu  in enumerate(menus) :
+    doc = str(index+1) +"번 메뉴 : "+ menu.get_text() +"\n"
+    file.write(doc)
+    print(doc)
+
+
