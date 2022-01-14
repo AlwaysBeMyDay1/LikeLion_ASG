36a9435f41bfa486d0ecced044f2db8a4ae538be
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-01-14 13:43:37 +0900

    api finished

--- 2022 LL/Python/inten/api.py
+++ 2022 LL/Python/inten/api.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 API_KEY = '1dbd59baf29415c1ff9bb3d4bfa229f4'
+lang = "kr"
 city = "Daegu"
-api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+units = "metric"
+api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang={lang}&units={units}"
 
 result = requests.get(api)
 print(type(result.text))
 
 data = json.loads(result.text)
 
-print(type(data))
+print(data)
+print(data["weather"][0]["main"])
