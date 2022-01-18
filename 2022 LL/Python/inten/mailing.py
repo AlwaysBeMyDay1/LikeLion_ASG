516112432892088b1eb425e3e0c6848008b9d60e
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-01-18 11:16:57 +0900

    정규표현식, file open

--- 2022 LL/Python/inten/mailing.py
+++ 2022 LL/Python/inten/mailing.py
@@ -1,6 +1,9 @@
 import imp
 import smtplib
 from email.message import EmailMessage
+import imghdr
+import re
+
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
@@ -15,7 +18,17 @@ message["Subject"] = "제목!!!"
 message["From"] = "eodus0901@gmail.com"
 message["To"] = "eodus09011@naver.com"
 
+# #이미지 파일 열기
+# img = open("gandalf_with_macbook.jpg","rb")
+# # 이미지 파일 읽어오기
+# print(img.read())
+# # 파일 열었으면 닫아줘야 함
+# img.close()
+with open("gandalf_with_macbook.jpg","rb") as img :
+    img_file = img.read()
 
+img_type = imghdr.what("gandalf_with_macbook",img_file)
+message.add_attachment(img_file,maintype='image',subtype='')
 
 # smtp 메일 서버 연결
 smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
@@ -23,7 +36,9 @@ smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
 # smtp 서버에 로그인
 print(smtp.login("eodus0901@gmail.com","start2020!"))
 
+#정규 표현식 사용
+reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+re.match(reg,"")
 # 메일 보내기
 smtp.send_message(message)
 smtp.quit()
-print("완료")
