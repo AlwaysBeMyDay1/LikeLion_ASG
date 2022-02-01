dd16e47c5028efdcfda99bc72107bedd5073395c
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-02-01 16:57:18 +0900

    user input with html form

--- 2022 LL/Django/Bamboo/blogproject/blog/views.py
+++ 2022 LL/Django/Bamboo/blogproject/blog/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
+from .models import Blog
+from django.utils import timezone
 
-# Create your views here.
 
 def home(req) :
     return render(req, "index.html")
+
+def new(req):
+    return render(req, "new.html")
+
+def create(req):
+    if(req.method == 'POST'):
+        post = Blog()
+        post.title = req.POST['title']
+        post.body = req.POST['body']
+        post.date = timezone.now()
+        post.save()
+    return redirect('home')
+
+def djangoform(req): 
+    return redirect('home')
+
+def modelform(req):
+    return redirect('home')
--- 2022 LL/Django/Bamboo/blogproject/blogproject/urls.py
+++ 2022 LL/Django/Bamboo/blogproject/blogproject/urls.py
@@ -20,4 +20,9 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('new/',views.new, name='new'),
+    path('create/',views.create, name='create'),
+
+    path("djangoform/", views.djangoform, name="djangoform"),
+    path("modelform/", views.modelform, name="modelform"),
 ]
